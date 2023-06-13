c5b586869410e1aa61da69a982117a0b0fb6d292
João Victor Nunes <jvmm.nunes@gmail.com>
2023-06-13 15:05:41 -0300

    adicionando sidebar

--- app.py
+++ app.py
@@ -39,13 +39,14 @@ def main():
     df_municipio = carrega_municipios()
 
     # Criando filtros
-    st.title("Menu")
-    st.markdown("Selecione os dados que deseja extrair")
-    doc_sel = st.selectbox("Selecione o documento", options=documentos)
-    anos_sel = st.multiselect("Selecione o periodo", options=range(2015, 2024))
-    mun_sel = st.multiselect("Selecione o ente", df_municipio.nome_municipio.unique())
-    esfera_sel = st.selectbox("Selecione a esfera", options=['M', 'E', 'U', 'C'])
-    anexo_sel = st.selectbox("Selecione o anexo", options=['01', '02', '03', '04', '05', '06', '07', '10'])
+    with st.sidebar:
+        st.title("Menu")
+        st.markdown("Selecione os dados que deseja extrair")
+        doc_sel = st.selectbox("Selecione o documento", options=documentos)
+        anos_sel = st.multiselect("Selecione o periodo", options=range(2015, 2024))
+        mun_sel = st.multiselect("Selecione o ente", df_municipio.nome_municipio.unique())
+        esfera_sel = st.selectbox("Selecione a esfera", options=['M', 'E', 'U', 'C'])
+        anexo_sel = st.selectbox("Selecione o anexo", options=['01', '02', '03', '04', '05', '06', '07', '10'])
 
     # Aplicando filtros de entes
     df_municipio = df_municipio[df_municipio['nome_municipio'].isin(mun_sel)]
